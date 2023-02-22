9e028507f6062ede2a8e1d165378efc4b2347e09
A8222668 <A8222668@g.pccu.edu.tw>
2023-02-23 00:43:04 +0800

    update section3

--- src/Slider.js
+++ src/Slider.js
@@ -2,20 +2,44 @@
 import './Slider.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
-import SwiftSlider from 'react-swift-slider';
+import Carousel from 'react-bootstrap/Carousel';
+//import SwiftSlider from 'react-swift-slider';
 
 function Slider() {
-	const images = [
+	/*const images = [
 		{'id':'1','src': require('./img/Carousel1.png')},
 		{'id':'2','src': require('./img/Carousel1.png')},
 		{'id':'3','src': require('./img/Carousel1.png')},
-	];
+	];*/
   return (
     <div className='Container2'>
-    	<SwiftSlider data={images} enableNextAndPrev={false} showDots={false}/>
+	  <Carousel controls={false}>
+		<Carousel.Item interval={5000}>
+			<img
+			className="d-block w-100"
+			src={require("./img/Carousel1.png")}
+			alt="First slide"
+			/>
+		</Carousel.Item>
+		<Carousel.Item interval={5000}>
+			<img
+			className="d-block w-100"
+			src={require("./img/Carousel1.png")}
+			alt="Second slide"
+			/>
+		</Carousel.Item>
+		<Carousel.Item interval={5000}>
+			<img
+			className="d-block w-100"
+			src={require("./img/Carousel1.png")}
+			alt="Third slide"
+			/>
+		</Carousel.Item>
+	  </Carousel>
+    	{/*<SwiftSlider data={images} enableNextAndPrev={false} showDots={false}/>*/}
     </div>
   );
 
 }
-//<SwiftSlider data={data} enableNextAndPrev={false} width={480}/>
+
 export default Slider;
