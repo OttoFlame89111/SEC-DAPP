76c8e789250d6cbc0b5ecb5dc59675fcfd4e5c15
A8222668 <A8222668@g.pccu.edu.tw>
2023-04-30 15:46:43 +0800

    寫入寵物資訊

--- src/views/addpet/index.jsx
+++ src/views/addpet/index.jsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
+import abi from './abi.json';
+
+const contractAddress = '0xfe3fA47C914D4Da0a829925BB82fb8095221FD97'
 
 function Addpet(){
+  const [value, setTheValue] = useState('');
+  const [name, setTheName] = useState('');
+  const [species, setTheSpecies] = useState('');
+  const [imageURL, setTheImageURL] = useState('');
+  
+  async function getValue() {
+    const provider = new ethers.providers.InfuraProvider('goerli');
+    const contract = new ethers.Contract(contractAddress, abi, provider);
+    const response = await contract.getUnadoptedPets();
+    setTheValue(response.length);
+    console.log(response);
+    console.log(response[1].name);
+  }
+  
+  async function setValue(name,species,imageURL) {
+    if (typeof window.ethereum !== 'undefined') {
+      await window.ethereum.enable();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      const tx = await contract.addPet(name,species,imageURL);
+      await tx.wait();
+      getValue();
+    }
+  }
+
+  let getTheName="";
+  let getTheSpecies="";
+  let getTheImageURL="";
+
     return(
         <form action="">
             <div style={{marginTop: 50}}
             ></div>
-            名字:<input type={Text}></input>
+            名字:<input type="text" onChange={e => getTheName=e.target.value} />
             <div style={{marginTop: 50}}
             ></div>
-            詳情介紹：<input type={Text}></input>
-            <div style={{marginTop: 50}}
-            ></div>
-
-            圖片 URL:<input type={Text}></input>
+            詳情介紹:<input type="text" onChange={e => getTheSpecies=e.target.value} />
             <div style={{marginTop: 50}}
             ></div>
 
-            <button type="button">送出</button>
+            圖片 URL:<input type="text" onChange={e => getTheImageURL=e.target.value} />
+            <div style={{marginTop: 50}}
+            ></div>
+
+            <button type="button" onClick={() => setValue(getTheName,getTheSpecies,getTheImageURL)}>送出</button>
             <div style={{marginTop: 200}}
             ></div>
 
